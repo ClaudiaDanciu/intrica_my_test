e1bc8cafdaefb104ae64e0e65dbfed2be04712f5
ClaudiaDanciu <contact@claudiadanciu.com>
2018-07-05 22:15:28 +0100

    t

--- index.php
+++ index.php
@@ -313,19 +313,19 @@
         <div class="row">
           <div class="col-md-6">
             <div class="form-group">
-              <input type="text" id="name" class="form-control" placeholder="Name" required="required" value="<?php echo $name; ?>" />
+              <input type="text" id="name" class="form-control" placeholder="Name" required="required" value="" />
               <p class="help-block text-danger"></p>
             </div>
           </div>
           <div class="col-md-6">
             <div class="form-group">
-              <input type="email" id="email" class="form-control" placeholder="Email" required="required" value="<?php echo $email; ?>" />
+              <input type="email" id="email" class="form-control" placeholder="Email" required="required" value="" />
               <p class="help-block text-danger"></p>
             </div>
           </div>
         </div>
         <div class="form-group">
-          <textarea name="message" id="message" class="form-control" rows="4" placeholder="Message" required ><?php echo $message; ?></textarea>
+          <textarea name="message" id="message" class="form-control" rows="4" placeholder="Message" required ></textarea>
           <p class="help-block text-danger"></p>
         </div>
         <div id="success"></div>
