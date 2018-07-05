83fca856ac6fad87e5f9511de7af8c3ad1d4a8c6
ClaudiaDanciu <contact@claudiadanciu.com>
2018-07-05 22:10:47 +0100

    t

--- contact-me.php
+++ contact-me.php
@@ -1,12 +1,11 @@
 <?php
 if(isset($_POST['email'])) {
  
-    // EDIT THE 2 LINES BELOW AS REQUIRED
     $email_to = "contact@claudiadanciu.com";
     $email_subject = "Your email subject line";
  
     function died($error) {
-        // your error code can go here
+        // error code can go here
         echo "We are very sorry, but there were error(s) found with the form you submitted. ";
         echo "These errors appear below.<br /><br />";
         echo $error."<br /><br />";
